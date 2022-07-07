df4e018537c2351a0d2482d09c0a6b708870c0d6
Ghrushnesh Rathod <ghrushneshr25@gmail.com>
2022-07-07 23:39:36 +0530

    Recursion

---
+++ Recursion/sortedArray.java
@@ -0,0 +1,22 @@
+/*
+    Problem-2 
+    Given an array, check whether the array is in sorted order with recursion.
+*/
+
+public class sortedArray {
+    public static boolean function(int[] array, int n) {
+        if (n == 0 || n == 1)
+            return true;
+
+        if (array[n - 1] < array[n - 2])
+            return false;
+
+        return function(array, n - 1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(function(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 7));
+        System.out.println(function(new int[] { 1, 3, 2, 4, 5, 6, 7 }, 7));
+    }
+}
---
+++ Recursion/towersOfHanoi.java
@@ -0,0 +1,36 @@
+/*
+    Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. 
+    The objective of the puzzle is to move the entire stack to another rod, 
+    obeying the following simple rules: 
+
+        - Only one disk can be moved at a time.
+        - Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack 
+            i.e. a disk can only be moved if it is the uppermost disk on a stack.
+        - No disk may be placed on top of a smaller disk.
+
+    Solution :
+    The pattern here is :
+        - Shift 'n-1' disks from 'A' to 'B', using C.
+        - Shift last disk from 'A' to 'C'.
+        - Shift 'n-1' disks from 'B' to 'C', using A.
+
+*/
+
+class towersOfHanoi {
+    public static void function(int n, char from, char to, char aux) {
+        if (n == 1) {
+            System.out.println("Move 1 disk from peg " + from + " to peg " + to);
+            return;
+        }
+
+        function(n - 1, from, aux, to);
+
+        System.out.println("Move disk from peg " + from + " to peg " + to);
+
+        function(n - 1, aux, to, from);
+    }
+
+    public static void main(String[] args) {
+        function(3, 'a', 'b', 'c');
+    }
+}
